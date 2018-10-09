ae211a495965e602768b2393ef0f29171e0c9a2d
Phakeernan Ratnacumar <Phakeernan.Ratnacumar@netcetera.com>
2018-10-09 17:10:47 +0200

    finished fifth exercise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  
+
   state = {
     text: ''
   }
-  
+
+  changeText(text) {
+    this.setState(() => ({
+      text: text
+    }))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-        <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
-          <p>{this.state.text}</p>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h1 className="App-title">ReactND - Coding Practice</h1>
+          </header>
+          <div className="container">
+            <input onChange={(event) => this.changeText(event.target.value)} value={this.state.text} type="text"
+                   placeholder="Say Something"/>
+            <p className="echo">Echo:</p>
+            <p>{this.state.text}</p>
+          </div>
         </div>
-      </div>
     );
   }
 }
